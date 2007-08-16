f39f61b1b3005b75592d745310514fe8175a9853
Mathias Ertl <mati@fsinf.at>
2007-08-16 10:41:15 +0000

    all things in trunk are now copied into the top-level

---
+++ customnavblocks.php
@@ -0,0 +1,19 @@
+<?php
+	
+   	$text = $this->translator->translate( 'CustomNavBlocks' );
+	$customblocks = explode ("\n", $text);
+
+    while(list($iarg, $ival) = each($customblocks))	{
+	
+		echo "\n<div class=\"portlet\" id=\"" . substr($ival,0,strpos($ival,"|")) . "\">\n";
+		echo "	<h5>" . substr($ival,strpos($ival,"|")+1,strlen($ival)) . "</h5>\n";
+		echo "	<div class=\"pBody\">\n";
+		
+		echo "	" . $this->msgWiki(substr($ival,0,strpos($ival,"|")));
+				
+		echo "	</div>\n";
+		echo "</div>\n\n";
+	}
+	echo "<br>\n";
+
+?>
