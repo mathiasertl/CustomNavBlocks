7286c1d1f87ceb944081b60ba4b5d3d019e0b12b
Mathias Ertl <mati@fsinf.at>
2007-09-14 17:26:25 +0000

    * renamed the CustomNavBlocks patch appropriatly * cleaned up CustomNavBlocks.php code (rename in next commit!) * renamed mReferer patch with appropriate extension

--- customnavblocks.php
+++ customnavblocks.php
@@ -1,19 +1,22 @@
 <?php
+/* this extension requires the CustomNavBlocks patch for MonoBook.php.
+ * You can get it at:
+ * 	http://svn.fsinf.at/mediawiki/patches/MonoBook.php/
+ */
 	
-   	$text = $this->translator->translate( 'CustomNavBlocks' );
-	$customblocks = explode ("\n", $text);
+$text = $this->translator->translate( 'CustomNavBlocks' );
+$customblocks = explode ("\n", $text);
 
-    while(list($iarg, $ival) = each($customblocks))	{
+while(list($iarg, $ival) = each($customblocks))	{
+	echo "\n<div class=\"portlet\" id=\"" . substr($ival,0,strpos($ival,"|")) . "\">\n";
+	echo "	<h5>" . substr($ival,strpos($ival,"|")+1,strlen($ival)) . "</h5>\n";
+	echo "	<div class=\"pBody\">\n";
 	
-		echo "\n<div class=\"portlet\" id=\"" . substr($ival,0,strpos($ival,"|")) . "\">\n";
-		echo "	<h5>" . substr($ival,strpos($ival,"|")+1,strlen($ival)) . "</h5>\n";
-		echo "	<div class=\"pBody\">\n";
-		
-		echo "	" . $this->msgWiki(substr($ival,0,strpos($ival,"|")));
-				
-		echo "	</div>\n";
-		echo "</div>\n\n";
-	}
-	echo "<br>\n";
+	echo "	" . $this->msgWiki(substr($ival,0,strpos($ival,"|")));
+			
+	echo "	</div>\n";
+	echo "</div>\n\n";
+}
+echo "<br>\n";
 
 ?>
