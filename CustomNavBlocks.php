31fcb082a4e2f9c1824c2becc0217ac458f54105
Mathias Ertl <mati@fsinf.at>
2008-08-16 21:00:43 +0000

    ups, forgot to delete a print statement

--- CustomNavBlocks.php
+++ CustomNavBlocks.php
@@ -1,23 +1,64 @@
 <?php
-/**
- * This extension requires the CustomNavBlocks patch for MonoBook.php.
- * You can get it at:
- * 	http://svn.fsinf.at/mediawiki/patches/MonoBook.php/
- */
-	
-$text = $this->translator->translate( 'CustomNavBlocks' );
-$customblocks = explode ("\n", $text);
-
-while(list($iarg, $ival) = each($customblocks))	{
-	echo "\n<div class=\"portlet\" id=\"" . substr($ival,0,strpos($ival,"|")) . "\">\n";
-	echo "	<h5>" . substr($ival,strpos($ival,"|")+1,strlen($ival)) . "</h5>\n";
-	echo "	<div class=\"pBody\">\n";
-	
-	echo "	" . $this->msgWiki(substr($ival,0,strpos($ival,"|")));
-			
-	echo "	</div>\n";
-	echo "</div>\n\n";
+# Alert the user that this is not a valid entry point to MediaWiki if they try to access the special pages file directly.
+if (!defined('MEDIAWIKI')) {
+	echo <<<EOT
+INSTRUCTIONS
+EOT;
+	exit( 1 );
 }
-echo "<br>\n";
+
+$wgHooks['SkinTemplateOutputPageBeforeExec'][] = 'addCustomNavBlocks';
+$wgExtensionCredits['other'][] = array (
+	'name' => 'CustomNavBlocks',
+	'description' => 'Better customization of your sidebar',
+	'version' => '2.0.0-1.13.0',
+	'author' => 'Mathias Ertl',
+	'url' => 'http://pluto.htu.tuwien.ac.at/devel_wiki/CustomNavBlocks',
+);
+
+function addCustomNavBlocks( $skin, $tpl ) {
+	$parserOptions = new ParserOptions();
+	global $wgParser;
+	
+	$CustomNavBlocksRaw = $tpl->translator->translate( 'CustomNavBlocks' );
+	$blocks = explode( "\n", $CustomNavBlocksRaw );
+	$sidebar = array();
+	
+	foreach ($blocks as $block) {
+		$tmp = explode( '|', $block );
+
+		# return false if a line in MediaWiki:CustomNavBlocks has more than one "|"
+		if ( count( $tmp ) != 2 )
+			return false;
+
+		# some shortcuts
+		$definition = $tmp[0];
+		$blockTitle = $tmp[1];
+
+		# first, we need a title object:
+		$title = Title::newFromText( $definition, NS_MEDIAWIKI );
+
+		# return false if a page defined by MediaWiki:CustomNavBlocks doesn't exist:
+		if ( ! $title->exists() )
+			return false;
+
+		# get article and content:
+		$article = new Article( $title );
+		$article->loadContent();
+		$content = $article->getContent();
+
+		# parse the mediawiki-syntax into html:
+		$content = $wgParser->preprocess( $content, $title, $parserOptions );
+		$parserOutput = $wgParser->parse( $content, $title, $parserOptions );
+		$html = $parserOutput->getText();
+
+		# make a sidebar block:
+		$sidebar["$blockTitle"] = $html;
+	}
+
+	# set sidebar to new thing:
+	$tpl->set( 'sidebar', $sidebar );
+	return true;
+}
 
 ?>
